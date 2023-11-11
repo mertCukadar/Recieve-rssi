f206d1cb564389fd5ef4675aba158b1f357307cf
cukadarMert <cukadar.mertkaan@gmail.com>
2023-11-12 00:08:55 +0300

    python rssi file spagetti code

--- recieveRssi.py
+++ recieveRssi.py
@@ -1,11 +1,62 @@
-#https://medium.com/macoclock/getting-a-list-of-nearby-wifi-access-points-in-python-on-macos-726d9fb59001
-#https://python.plainenglish.io/estimating-distance-using-rssi-python-8f3d44c38108
-
 import subprocess
+import pandas as pd
+from IPython.display import display
+import matplotlib.pyplot as plt
 
 
-scan_cmd = subprocess.Popen(['airport', '-I'], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-scan_out, scan_err = scan_cmd.communicate()
+request_count = 1000
+data = {}
+is_defined = False
 
-print(scan_out)
-print(scan_err)
+for i in range(request_count):
+    scan_cmd = subprocess.Popen(['airport', '-I'], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    scan_out, scan_err = scan_cmd.communicate()
+
+    # Byte veriyi string veriye çevirelim.
+    scan_out = scan_out.decode('utf-8')
+    lines = scan_out.split('\n')
+
+    # build key values for dict
+    if is_defined == False:
+        for line in lines:
+            try:
+                key , value = line.strip().split(':')
+                data[key] = []
+            except ValueError:
+                continue
+        is_defined = True
+
+    # build values for dict
+    if is_defined == True:
+        for line in lines:
+            try:
+                key , value = line.strip().split(':')
+                data[key].append(value)
+            except ValueError:
+                continue
+
+# Pandas DataFrame oluşturun
+data_frame = pd.DataFrame(data)
+
+pd.set_option('display.max_rows', None)
+pd.set_option('display.max_columns', None)
+pd.set_option('display.width', None)
+
+
+pass_key = ['state', 'op_mode', 'lastAssocStatus', '802.11 auth', 'link auth', 'BSSID', 'SSID', 'MCS', 'channel', 'HT', 'CC' , "op mode" , "" , " "]
+#subplots for every key
+figure_count = len(data.keys())-len(pass_key)
+fig, axs = plt.subplots(figure_count, figsize=(15, 15))
+
+
+for i, key in enumerate(data.keys()):
+    if i >= figure_count:
+        break
+    if key not in pass_key:
+        axs[i].plot(data[key])
+        axs[i].set_title(key)
+        axs[i].set_xlabel('request count')
+        axs[i].set_ylabel(key)
+        axs[i].grid(True)
+
+plt.show()
